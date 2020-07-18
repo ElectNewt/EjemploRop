ec5d73ecebb739e6652705b83b336447c7001e2b
ElectNewt <ivanxo69@gmail.com>
2020-07-18 16:21:44 +0100

    add result ignore

---
+++ src/ROP/Result_Ignore.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+using System.Threading.Tasks;
+
+namespace ROP
+{
+    public static class Result_Ignore
+    {
+
+        public static Result<Unit> Ignore<T>(this Result<T> r)
+        {
+            try
+            {
+                return r.Success
+                    ? Result.Success()
+                    : Result.Failure(r.Errors);
+            }
+            catch (Exception e)
+            {
+                ExceptionDispatchInfo.Capture(e).Throw();
+                throw;
+            }
+        }
+
+        public static async Task<Result<Unit>> Ignore<T>(this Task<Result<T>> r)
+        {
+            try
+            {
+                return (await r).Ignore();
+            }
+            catch (Exception e)
+            {
+                ExceptionDispatchInfo.Capture(e).Throw();
+                throw;
+            }
+        }
+    }
+}
