d31d382b35275577091852956d81c49227c299ea
ElectNewt <ivanxo69@gmail.com>
2021-02-24 19:03:39 +0000

    task1: add fallback V1

---
+++ src/ROP/Result_Fallback.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+using System.Threading.Tasks;
+
+namespace ROP
+{
+    public static class Result_Wrapper
+    {
+        public static Result<U> Wrapper<T, U>(this Result<T> r, Func<T, Result<U>> method)
+        {
+            try
+            {
+                return r.Success
+                    ? method(r.Value)
+                    : Result.Failure<U>(r.Errors);
+            }
+            catch (Exception e)
+            {
+                ExceptionDispatchInfo.Capture(e).Throw();
+                throw;
+            }
+
+        }
+     }
+
+
+    public static class Result_Fallback
+    {
+        public static Result<T> Fallback<T>(this Result<T> r, Func<T, Result<T>> method)
+        {
+            try
+            {
+                return r.Success
+                    ? r.Value
+                    : method(r.Value);
+                
+            }
+            catch (Exception e)
+            {
+                ExceptionDispatchInfo.Capture(e).Throw();
+                throw;
+            }
+        }
+
+        public static async Task<Result<T>> Fallback<T>(this Task<Result<T>> r, Func<T, Task<Result<T>>> method)
+        {
+            try
+            {
+                var result = await r;
+                return result.Success
+                    ? result.Value
+                    : await method(result.Value);
+
+            }
+            catch (Exception e)
+            {
+                ExceptionDispatchInfo.Capture(e).Throw();
+                throw;
+            }
+        }
+
+
+    }
+}
---
+++ test/ROP.UnitTest/TestFallback.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ROP.UnitTest
+{
+    public class TestFallback
+    {
+
+        [Fact]
+        public void TestFallbackFAllsInsideFallback()
+        {
+            var result =
+                MetodoOriginal(1)
+                .Bind(x => 
+                    MetodoQueFalla(x)
+                    .Fallback(_=>MetodoQueDevuelveNumeroDeMeses(x))
+                    );
+
+            Assert.True(result.Success);
+            Assert.Equal(12, result.Value);
+
+        }
+
+
+        [Fact]
+        public void TestFallbackWhenMethodDoesntFail()
+        {
+            var result =
+                MetodoOriginal(2)
+                .Bind(x =>
+                    MatodoQueMultiplica(x)
+                    .Fallback(_ => MetodoQueDevuelveNumeroDeMeses(x))
+                    );
+
+            Assert.True(result.Success);
+            Assert.Equal(4, result.Value);
+        }
+
+        private Result<int> MetodoOriginal(int i)
+        {
+            return i;
+        }
+
+        private Result<int> MetodoQueFalla(int i)
+        {
+            return Result.Failure<int>("error");
+        }
+
+        private Result<int> MatodoQueMultiplica(int i)
+        {
+            return i * i;
+        }
+
+        private Result<int> MetodoQueDevuelveNumeroDeMeses(int i)
+        {
+            return 12;
+        }
+
+    }
+}
